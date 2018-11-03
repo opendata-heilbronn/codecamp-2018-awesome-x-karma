6115d13e13518dcc4c11130ae2532b91ab3b9573
Marcel Michelfelder <marcel.michelfelder@uni-ulm.de>
2018-11-03 23:09:23 +0100

    Added easing to animation

--- src/app/components/receipt-info/receipt-info.component.ts
+++ src/app/components/receipt-info/receipt-info.component.ts
@@ -13,15 +13,27 @@ export class ReceiptInfoComponent implements OnInit {
   @Input()
   description: string = '';
 
+  // in ms
+  private animationDuration = 1000;
+
   constructor() {}
 
   ngOnInit() {
+    let start = Date.now();
     let intervalHandle = setInterval(() => {
-      if (this.progress > this.currentProgress) {
-        this.currentProgress++;
-      } else {
-        clearInterval(intervalHandle);
-      }
+      let time = Date.now() - start;
+      this.currentProgress = this.easeInOutQuart(
+        time,
+        0,
+        this.progress - 0,
+        this.animationDuration
+      );
+      if (time >= this.animationDuration) clearInterval(intervalHandle);
     }, 20);
+  }
+
+  easeInOutQuart(t, b, c, d) {
+    if ((t /= d / 2) < 1) return (c / 2) * t * t * t * t + b;
+    return (-c / 2) * ((t -= 2) * t * t * t - 2) + b;
   }
 }
