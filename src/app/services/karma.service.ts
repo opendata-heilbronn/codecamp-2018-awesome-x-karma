0011ccf9437be97e1c4914882984958e9ffbb3e0
Eugen Geisler <eugen@gutco.de>
2018-11-04 11:07:17 +0100

    Add WIP of karma service

--- src/app/services/karma.service.ts
+++ src/app/services/karma.service.ts
@@ -6,8 +6,48 @@ import { Injectable } from '@angular/core';
 export class KarmaService {
   constructor() {}
 
+  kmScale(product: any): number {
+      let score = 0;
+      if (product.distance === null) {
+          return null;
+      }
+      if (product.distance <= 30) {
+          score = 40;
+      } else if (product.distance <= 50) {
+          score = 30;
+      } else if (product.distance <= 80) {
+          score = 20;
+      } else if (product.distance <= 150) {
+          score = 10;
+      } else if (product.distance <= 300) {
+          score = 3;
+      }
+      return score
+  }
+
+  haltung(product: any): number {
+      let score = 0;
+      if (product.haltung === null) {
+          return null;
+      }
+      if (product.haltung === "bio") {
+          score = 50;
+      } else if (product.haltung === "freiland") {
+          score = 25;
+      } else if (product.haltung === "fairtrade") {
+          score = 40;
+      }
+      return score
+  }
+
   GetKarma(product: any): number {
-    // ToDo: ...
-    return 4;
+    let score: number = 0
+    if (product.category === "o") {
+        score += this.kmScale(product)
+        score += this.haltung(product)
+    } else if (product.category === "t") {
+
+    }
+    return score;
   }
 }
